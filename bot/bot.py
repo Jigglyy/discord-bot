9761e5c59760aa9960e55bae5f67d629d0e77ea6
Jiggly <Remikzfully@gmail.com>
2023-02-25 19:23:21 +0200

    added image recognition & delete matching

--- bot/bot.py
+++ bot/bot.py
@@ -5,14 +5,16 @@ import logging
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
-from commands import example_command
 from data import database
 from data import data_handler
 import discord
 from discord.ext import commands
-import sqlite3
 from loguru import logger
 import datetime
+import uuid
+from PIL import Image
+from pixelmatch.contrib.PIL import pixelmatch
+from discord.errors import ConnectionClosed
 
 # Setting up varibles
 
@@ -20,6 +22,7 @@ config = data_handler.read_file("config.json")
 token = config['token']
 prefix = config['prefix']
 description = config['description']
+bot_id = config['bot_id']
 
 # Call create_table to create the users table
 
@@ -47,13 +50,52 @@ handler.setLevel(logging.DEBUG)
 @bot.event
 async def on_ready():
     logger.info(f'We have logged in as {bot.user}')
-    
+
 @commands.command()
 async def ping(ctx):
     logger.info("Heard Ping, sending Pong")
     await ctx.send("Pong")
+
+@bot.event
+async def on_message(msg):
+    try:
+        channel = msg.channel
+        uuid_gen = str(uuid.uuid4())
+        today = datetime.date.today().strftime("%Y-%m-%d")
+        image_name = f"{today}_{uuid_gen}.png"
+        await msg.attachments[0].save(image_name)
+        
+        try:
+            img_a = Image.open("image.jpg")
+            img_b = Image.open(image_name)
+            img_diff = Image.new("RGBA", img_a.size)
+
+            mismatch = pixelmatch(img_a, img_b, img_diff, includeAA=True)
+            logger.info(mismatch)
+
+            img_diff.save("diff.png")
+            
+            if mismatch > 300_000:
+                await msg.delete()
+                await channel.send(f"Your picture contains foul imagery, it has been deleted!")
+                
+        except ValueError as e:
+            logger.error(str(e))
+        
+
+    except IndexError:
+        if msg.author.id == bot_id:
+            logger.info("Detected bot message in channel: " + str(msg.channel.name) + ", ignoring")
+        else:
+            logger.error("Index Error: No Attachments")
+
+
+# Add Commands
+
 bot.add_command(ping)
 
 # Run Bot
-
-bot.run(token, log_handler=handler)
+try:
+    bot.run(token, log_handler=handler)
+except ConnectionClosed as e:
+    logger.error(str(e))
